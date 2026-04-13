44686ec5c85b183679276d2df5b12246a8ce36cc
Vishwaraj Singh Shekhawat <vishwaraj.singh1523@gmail.com>
2026-04-13 12:10:18 +0530

    Update image source paths in Dock component to use BASE_URL

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './Dock.scss';
-import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const Dock = ({ windowsState, setwindowsState }) => {
   return (
     <footer className='dock'>
-      <div onClick={() => { setwindowsState(state => ({ ...state, github: true })) }} className='icon github'><img src="public\dockIcons\github.svg" alt="" /></div>
+      <div onClick={() => { setwindowsState(state => ({ ...state, github: true })) }} className='icon github'><img src={`${import.meta.env.BASE_URL}dockIcons/github.svg`} /></div>
 
       <div onClick={() => {
         window.open("https://calendar.google.com/calendar/u/0/r?pli=1","_blank")
-      }} className='icon calender'><img src="public\dockIcons\calender.svg" alt="" /></div>
+      }} className='icon calender'><img src={`${import.meta.env.BASE_URL}dockIcons/calender.svg`} />
+</div>
 
       <div onClick={() => {
         window.open("https://www.linkedin.com/in/vishwarajsinghshekhawat/", "_blank")
-      }} className='icon link'><img src="public\dockIcons\link.svg" alt="" /></div>
+      }} className='icon link'><img src={`${import.meta.env.BASE_URL}dockIcons/link.svg`} />
+</div>
 
       <div onClick={() => {
         window.open("mailto:vishwaraj.singh1523@gmail.com", "_blank")
-      }} className='icon mail'><img src="public\dockIcons\mail.svg" alt="" /></div>
+      }} className='icon mail'><img src={`${import.meta.env.BASE_URL}dockIcons/mail.svg`} />
+</div>
       
-      <div onClick={() => { setwindowsState(state => ({ ...state, note: true })) }} className='icon note'><img src="public\dockIcons\note.svg" alt="" /></div>
-      <div onClick={() => { setwindowsState(state => ({ ...state, resume: true })) }} className='icon pdf'><img src="public\dockIcons\pdf.svg" alt="" /></div>
-      <div onClick={() => { setwindowsState(state => ({ ...state, spotify: true })) }} className='icon spotify'><img src="public\dockIcons\spotify.svg" alt="" /></div>
-      <div onClick={() => { setwindowsState(state => ({ ...state, cli: true })) }} className='icon cli'><img src="public\dockIcons\cli.svg" alt="" /></div>
+      <div onClick={() => { setwindowsState(state => ({ ...state, note: true })) }} className='icon note'><img src={`${import.meta.env.BASE_URL}dockIcons/note.svg`} />
+</div>
+      <div onClick={() => { setwindowsState(state => ({ ...state, resume: true })) }} className='icon pdf'><img src={`${import.meta.env.BASE_URL}dockIcons/pdf.svg`} />
+</div>
+      <div onClick={() => { setwindowsState(state => ({ ...state, spotify: true })) }} className='icon spotify'><img src={`${import.meta.env.BASE_URL}dockIcons/spotify.svg`} />
+</div>
+      <div onClick={() => { setwindowsState(state => ({ ...state, cli: true })) }} className='icon cli'><img src={`${import.meta.env.BASE_URL}dockIcons/cli.svg`} /></div>
     </footer>
   );
 };
